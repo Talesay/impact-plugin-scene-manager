cb0278fc21e47ac58edfc77f2024852cced23a1b
FelipeBudinich <fbudinichd@gmail.com>
2015-02-18 19:33:54 -0300

    Added license to EOF

--- manager.js
+++ manager.js
@@ -1,3 +1,24 @@
+/**
+# Impact JS Scene Manager
+
+This plugins extends the native Array object with non-enumerable utility methods
+
+__Installation__
+
+As a submodule, from the git command line:
+
+```
+git submodule add https://github.com/Talesay/impact-plugin-scene-manager.git lib/plugins/scene
+``` 
+
+Then require this module:
+
+```
+'plugins.scene.manager'
+``` 
+
+__JSLint Flags__
+*/
 /*global ig*/
 ig.module(
     'plugins.scene.manager'
@@ -30,3 +51,25 @@ ig.module(
     });
     return new ig.SceneManager();
 });
+/**
+ * # The MIT License (MIT)
+ * ## Copyright (c) 2015 Talesay
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
